39774a3a52514233dfa3bc490439ac90053839be
Hangyu Ye <jerry.hangyu@gmail.com>
2018-08-01 23:27:51 +0100

    Initial Commit

---
+++ SoftChain Wallet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SoftChain Wallet
+//
+//  Created by Jerry on 2018/8/1.
+//  Copyright © 2018年 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
