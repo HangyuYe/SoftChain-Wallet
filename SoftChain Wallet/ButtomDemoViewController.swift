8276d2bb7aac29146d940c23c65f16eea6d07c54
Hangyu Ye <jerry.hangyu@gmail.com>
2018-08-06 00:00:43 +0100

    Files Update

--- SoftChain Wallet/ButtomDemoViewController.swift
+++ SoftChain Wallet/ButtomDemoViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ButtomDemoViewController.swift
 //  SoftChain Wallet
 //
 //  Created by Jerry on 2018/8/1.
@@ -8,19 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ButtomDemoViewController: UIViewController {
 
     
     @IBOutlet weak var SendBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @IBAction func sendBtnPressed(_ sender: Any) {
