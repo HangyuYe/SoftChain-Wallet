4e62a077db332ee2ea67b306e9eacadaed331e7f
Hangyu Ye <jerry.hangyu@gmail.com>
2018-08-06 01:27:04 +0100

    Basic Strc. Set

---
+++ SoftChain Wallet/WelcomeScreen/WelcomeViewController.swift
@@ -0,0 +1,55 @@
+//
+//  WelcomeViewController.swift
+//  SoftChain Wallet
+//
+//  Created by Jerry on 2018/8/6.
+//  Copyright © 2018年 SoftChain Foundation Ltd. All rights reserved.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
